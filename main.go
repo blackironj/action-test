4e3d365f6d5b1dc79a52abfda27df87d9cd5c4bc
Hyun <blackironj@gmail.com>
2021-01-15 18:19:21 +0900

    Update k8s config file and server code

--- main.go
+++ main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	router := gin.Default()
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
-	router.GET("/health", func(c *gin.Context) {
+func main() {
+	r := gin.Default()
+	r.LoadHTMLFiles("index.html")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	r.GET("/health", func(c *gin.Context) {
 		c.String(http.StatusOK, "I'm okay")
 	})
 
-	// This handler will match /user/john but will not match /user/ or /user
-	router.GET("/user/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(http.StatusOK, "Hello %s", name)
+	r.GET("/ws", func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
 	})
 
-	router.Run(":8080")
+	r.Run("localhost:12312")
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	defer conn.Close()
+	if err != nil {
+		fmt.Println("Failed to set websocket upgrade: ", err)
+		return
+	}
+
+	for {
+		t, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("disconnected")
+			break
+		}
+		conn.WriteMessage(t, msg)
+	}
 }
